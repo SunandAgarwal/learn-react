f26cb81896bee4962606fe8aabe6c1cdc85a1432
Sunand Agarwal <sunandagarwal@Sunands-MacBook-Air.local>
2024-02-09 16:58:07 -0500

    fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,18 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
+// import Gallery from './components/gallery'
+// import Profile_mistake from './qcomps/profile_mistake'
+// import FirstComp from './qcomps/firstcomp'
+// import Bios from './qcomps/bios'
+// import Todos from './qcomps/todos'
+// import ProfileProps from './components/profile_props';
+import Gallery from './qcomps/gallery_props'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <Gallery />
     </div>
   )
 }
--- src/pages/qcomps/gallery_props.js
+++ src/pages/qcomps/gallery_props.js
@@ -1,53 +1,70 @@
+function Image({src, alt, size=100}) {
+  return (
+    <img
+          className="avatar"
+          src={src}
+          alt={alt}
+          width={size}
+          height={size}
+        />
+  )
+}
+
+function ListElem({Head, content}) {
+  return (
+      <li>
+        <b>{Head}</b>
+        {content}
+      </li>
+  )
+}
+
 export default function Gallery() {
   return (
     <div>
       <h1>Notable Scientists</h1>
       <section className="profile">
         <h2>Maria Skłodowska-Curie</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/szV5sdGs.jpg'
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
+        <Image
+          src = {'https://i.imgur.com/szV5sdGs.jpg'}
+          alt = {"Maria Skłodowska-Curie"}
+          size = {70}
         />
         <ul>
-          <li>
-            <b>Profession: </b>
-            physicist and chemist
-          </li>
-          <li>
-            <b>Awards: 4 </b>
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            polonium (element)
-          </li>
+          <ListElem
+            Head = {"Profession: "}
+            content = {"physicist and chemist"}
+          />
+          <ListElem
+            Head = {"Awards: 4 "}
+            content = {"(Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)"}
+          />
+          <ListElem
+            Head = {"Discovered: "}
+            content = {"polonium (element)"}
+          />
         </ul>
       </section>
       <section className="profile">
         <h2>Katsuko Saruhashi</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/YfeOqp2s.jpg'
-          alt="Katsuko Saruhashi"
-          width={70}
-          height={70}
+        <Image
+          src = {'https://i.imgur.com/YfeOqp2s.jpg'}
+          alt = {"Katsuko Saruhashi"}
+          size = {70}
         />
         <ul>
-          <li>
-            <b>Profession: </b>
-            geochemist
-          </li>
-          <li>
-            <b>Awards: 2 </b>
-            (Miyake Prize for geochemistry, Tanaka Prize)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            a method for measuring carbon dioxide in seawater
-          </li>
+          <ListElem
+            Head = {"Profession: "}
+            content = {"geochemist"}
+          />
+          <ListElem
+            Head = {"Awards: 2 "}
+            content = {"(Miyake Prize for geochemistry, Tanaka Prize)"}
+          />
+          <ListElem
+            Head = {"Discovered: "}
+            content = {"a method for measuring carbon dioxide in seawater"}
+          />
         </ul>
       </section>
     </div>
